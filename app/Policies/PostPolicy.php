f5a8bd0abddc68d288c157533c85b62222d7f8b8
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-04-25 18:08:55 +0900

    refactor(auth): implement authorization checks for post actions and simplify post model methods

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -12,7 +12,7 @@ class PostPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -20,7 +20,7 @@ class PostPolicy
      */
     public function view(User $user, Post $post): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -28,7 +28,7 @@ class PostPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
