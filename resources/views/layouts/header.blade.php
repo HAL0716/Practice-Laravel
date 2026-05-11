7fc8601b1b6731a0e1166f3f354ea17dd30b9f56
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-05-11 09:32:47 +0900

    fix(header): update text color for application name in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('posts.index') }}" class="flex items-center gap-2">
                         <x-application-logo class="h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                        <span class="font-bold text-lg">
+                        <span class="font-bold text-lg text-gray-800 dark:text-gray-200">
                             {{ config('app.name', 'Laravel') }}
                         </span>
                     </a>
